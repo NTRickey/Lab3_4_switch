8277bde0ca0c48f86558b174fba42de3ad81c7b3
Никита <@DESKTOP-0N74OFC>
2023-10-15 18:35:27 +0700

    Добавьте файлы проекта.

---
+++ Lab3_4_switch/Lab3_4_switch.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>;
+#include<locale>;
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	int day;
+	cout << "Введите номер дня недели: ";
+	cin >> day;
+	switch (day)
+	{
+	case 1: cout << "Понедельник";
+		break;
+	case 2: cout << "Вторник";
+		break;
+	case 3: cout << "Среда";
+		break;
+	case 4: cout << "Четверг";
+		break;
+	case 5: cout << "Птяница";
+		break;
+	case 6: cout << "Суббота";
+		break;
+	case 7: cout << "Воскресенье";
+		break;
+	default:
+		cout << "В неделе только 7 дней";
+	}
+}
